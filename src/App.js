0b2e2f0a48e6ffc0070b795a46b132a95631f3d7
akhilesh216 <akhilesh216@gmail.com>
2022-06-01 15:52:13 +0530

    Dynamic Columns

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-
-
 function App() {
-  var Search = [
+  var Feed = [
     {
+      id: 1,
       CarName: "Nexon",
       Brand: "Tata",
       Mileage: 20,
       Electric: true,
       Safety: 16,
+      
     },
     {
+      id: 2,
       CarName: "Venue",
       Brand: "Hyundai",
       Mileage: 15,
@@ -20,6 +21,7 @@ function App() {
       Safety: 12,
     },
     {
+      id: 3,
       CarName: "XUV300",
       Brand: "Mahindra",
       Mileage: 16,
@@ -27,48 +29,58 @@ function App() {
       Safety: 14,
     },
     {
+      id: 4,
       CarName: "Brezza",
       Brand: "Maruti Suzuki",
       Mileage: 18,
       Electric: false,
       Safety: 14,
+      
     },
+    
+    
+  ];
+  var FeedColHead=[
+      "#","CarName","Brand","Mileage","Electric","Safety (Out of 17)","abcc",
   ];
   
+
   return (
     <>
-    
-    <h1 className="title-main">Car Search Data </h1>
-    <table className="cars-table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">CarName</th>
-          <th scope="col">Brand</th>
-          <th scope="col">Mileage</th>
-          <th scope="col">Electric</th>
-          <th scope="col">Safety (Out of 17)</th>
-        </tr>
-      </thead>
-      
-      <tbody>
-{
-  Search.map((car,index)=>{
-      return (
-        <tr>
-          <td scope="row">{index+1}</td>
-          <td>{car.CarName}</td>
-          <td>{car.Brand}</td>
-          <td>{car.Mileage}</td>
-          <td>{car.Electric?'YES':'NO'}</td>
-          <td>{car.Safety}</td>
-        </tr>
-      )
-  })
-}
-        
-      </tbody>
-    </table>
+      <h1 className="title-main">Car Search Data </h1>
+      <table className="cars-table">
+        <thead>
+       <tr>
+         {
+         FeedColHead.map((col)=>{
+return(<th scope="col">{col}</th>)
+         })
+         }
+       </tr>
+        </thead>
+
+        <tbody>
+          {Feed.map((car, index) => {
+            return (
+              <tr>
+                {
+                  Object.keys(car).map((cell)=>{
+                    if(typeof(car[cell])=='boolean')
+                    {
+                    return(<td>{car[cell] ? "YES" : "NO"}</td>)
+                    }
+else
+                   return( <td>{car[cell]}</td>)
+                
+                  })
+                }
+                
+              </tr>
+            );
+          })}
+          
+        </tbody>
+      </table>
     </>
   );
 }
