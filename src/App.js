6035729d297493c5a6f04502dbb9b2200eed687c
akhilesh216 <akhilesh216@gmail.com>
2022-06-05 22:13:40 +0530

    addFile

--- src/App.js
+++ src/App.js
@@ -1,74 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { db } from './firebase';
+
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import List from './List'
+import Form from './Form'
+import { Routes, Route, Link } from "react-router-dom";
+
 
 
 function App() {
-  const [cars, setCars] = useState([]);
  
-
-  var Feed = [];
-  var FeedColHead = [
-    "#", "Brand", "Mileage" ,"Electric","Safety (Out of 17)","CarName",  "Image" , "ID"
-  ];
-  async function getCars() {
-    let data = await axios.get('https://62991f6f7b866a90ec3722e2.mockapi.io/Cars');
-    console.log(data.data);
-    setCars(data.data);
-
-
-  }
-
-  useEffect(() => {
-    // Update the document title using the browser API
-
-    // start();
-    // getCars();
-  });
   return (
     <>
-      <h1 className="title-main">Car Search Data </h1>
-      <table className="cars-table">
-        <thead>
-          <tr>
-            {
-              FeedColHead.map((col) => {
-                return (<th scope="col">{col}</th>)
-              })
-            }
-          </tr>
-        </thead>
-
-        <tbody>
-          {cars.map((car, index) => {
-            return (
-              <tr>
-                <td>{index}</td>
-               
-                {
-                  Object.keys(car).map((cell)=>{
-                    if(cell == "image"){
-                      return( <td><img className="image" src={car[cell]}></img></td>)
-                    }
-                    else if(typeof(car[cell])=='boolean')
-                    {
-                    return(<td>{car[cell] ? "YES" : "NO"}</td>)
-                    }
-else
-                   return( <td>{car[cell]}</td>)
-                
-                  })
-                }
-
-              </tr>
-            );
-          })}
-
-        </tbody>
-      </table>
-      <button type="button" onClick={getCars}>Click me!</button>
+    <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="add" element={<Form />} />
+      </Routes>
+     
     </>
   );
 }
